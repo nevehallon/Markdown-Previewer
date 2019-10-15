7d9387bba4011324a0b2debc6d0c95787d119111
Neveh Allon <NevehAllon@gmail.com>
2019-10-15 15:28:33 +0300

    Updates

---
+++ precache-manifest.90ffa511ee312bd7adba86bc4765b4a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64d6169d8893e654ce290edb379fb93c",
+    "url": "/markdown-previewer/public/index.html/index.html"
+  },
+  {
+    "revision": "242c991f38081167f11a",
+    "url": "/markdown-previewer/public/index.html/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d1eefd33cc5c974da29a",
+    "url": "/markdown-previewer/public/index.html/static/js/2.57accd9f.chunk.js"
+  },
+  {
+    "revision": "242c991f38081167f11a",
+    "url": "/markdown-previewer/public/index.html/static/js/main.86d421b1.chunk.js"
+  },
+  {
+    "revision": "a3179c3e2738bfb7f183",
+    "url": "/markdown-previewer/public/index.html/static/js/runtime-main.410a4ee2.js"
+  }
+]);
